c5569e7d2adc677330a149f43ac62e8046da2dc9
Jorge Muñiz <61331463+JMunizDeveloper@users.noreply.github.com>
2020-04-14 16:18:47 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$("#alt_cost_btn").on("click", function (event) {
+    $("#alt_cost_desc").toggle("200");
+});
+
+$("#comp_btn").on("click", function (event) {
+    $("#comp_desc").toggle("200");
+});
+
+$("#confecc_btn").on("click", function (event) {
+    $("#confecc_desc").toggle("200");
+});
+
+$("#diseno_btn").on("click", function (event) {
+    $("#diseno_desc").toggle("200");
+});
+
+$("#manualidades_btn").on("click", function (event) {
+    $("#manualidades_desc").toggle("200");
+});
+
+$("#accesorios_btn").on("click", function (event) {
+    $("#accesorios_desc").toggle("200");
+});
